c34ad34df0b8ea7985b68fe68932dcee91655559
Jake Jenkins <jakejenkins2222@gmail.comm>
2025-09-19 21:39:56 -0600

    Created grade letter determining w/ pass or fail message

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade_percentage = Console.ReadLine();
+        int number = int.Parse(grade_percentage);
+
+        string grade_letter = "";
+
+
+        if (number >= 90)
+        {
+            grade_letter = "A";
+        }
+        else if (number >= 80)
+        {
+            grade_letter = "B";
+        }
+        else if (number >= 70)
+        {
+           grade_letter = "C";
+        }
+        else if (number >= 60)
+        {
+            grade_letter = "D";
+        }
+        else if (number < 60)
+        {
+            grade_letter = "F";
+        }
+
+        Console.WriteLine($"You have an {grade_letter}!");
+        
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed. Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time!");
+        }
+
+
     }
 }
