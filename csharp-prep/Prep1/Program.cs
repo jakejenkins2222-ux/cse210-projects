0e93c62bc63be82dd5adf49f3835aa8e698f16bd
Jake Jenkins <jakejenkins2222@gmail.comm>
2025-09-19 21:09:26 -0600

    Get name, display name like 007

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
+
+
+//What is your first name? Scott
+//What is your last name? Burton
+
+//Your name is Burton, Scott Burton.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        
     }
 }
