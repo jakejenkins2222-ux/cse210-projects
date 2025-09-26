7fe80083e575fe5e8f4098f2aa47bcaef13915d6
Jake Jenkins <jakejenkins2222@gmail.comm>
2025-09-25 20:36:29 -0600

    wrote guessing game program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //string input = Console.ReadLine();
+        //int number = int.Parse(input);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        // Console.WriteLine($"Your number is {number}");
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
