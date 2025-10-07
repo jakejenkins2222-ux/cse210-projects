995232ae10ec45849862789df137ba03cdc9e194
Jake Jenkins <jakejenkins2222@gmail.comm>
2025-10-06 22:41:21 -0600

    Created a job/resume with objects, classes, behaviors

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //job1.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._personName = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
+
+
     }
 }
